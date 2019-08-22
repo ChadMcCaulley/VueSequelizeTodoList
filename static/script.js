781d678612ce8f29a8448cc8f17e670cfb5dae76
Emily Laffrey <emily.laffrey@pgm-corp.com>
2019-08-22 11:24:45 -0700

    change color and and post new response

--- static/script.js
+++ static/script.js
@@ -2,41 +2,55 @@ var app = new Vue({
     el: '#app',
     methods: {
         colorChange: function (task) {
-            if (task.color == 'black') {
+            if (task.color == '#FFFFFF') {
                 task.color = 'red';
             } else if (task.color == 'red') {
                 task.color = 'green';
             } else if (task.color == 'green') {
                 task.color = 'blue';
-            } else  {
-                task.color = 'black';
+            } else {
+                task.color = '#FFFFFF';
             }
 
         },
         deleteLine: function (taskIndex) {
             this.list.splice(taskIndex, 1)
         },
+
         add: function () {
+            let parentThis = this;
             if (this.newTask !== '') {
+                axios.post('/api/todo', { text: parentThis.newTask })
+                    .then(function (response) {
+                        console.log(response);
+                        parentThis.addTodo(response.data)
+                        parentThis.newTask = ''
+
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+            /*if (this.newTask !== '') {
                 let newTask = {
                     text: this.newTask,
                     color: 'black',
                     readonly: true
                 }
                 this.list.push(newTask)
-                this.newTask = ''
-            }
-        },
-        contentEdit: function (task) {
-            task.readonly = !task.readonly
-        },
-        addTodo: function(todo){
-            todo.readonly = true;
-            this.list.push(todo)
-            console.log('addTodo');
-        }
-
+                this.newTask = '' */
+        
     },
+    contentEdit: function (task) {
+        task.readonly = !task.readonly
+    },
+    addTodo: function (todo) {
+        todo.readonly = true;
+        this.list.push(todo)
+        console.log('addTodo');
+    }
+
+},
     mounted: function () {
         let parentThis = this;
 
